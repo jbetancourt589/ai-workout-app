3e960cbf6080b9faa036a9a0dc179650539e099d
duranmichael681 <duranmichael681@gmail.com>
2025-08-12 15:26:06 -0400

    Added a landing page with a title and image.

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,76 @@
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+export default function Landing() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <View><Text style={styles.title}>Kraft</Text></View>
+      </View>
+      <Image
+        source={require('../../assets/images/landing-image.png')}
+        style={{ width: 300, height: 300, marginBottom: 20 }}
+      />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Enter your email" style={styles.input} />
+        <TextInput placeholder="Enter your password" style={styles.input} secureTextEntry />
+      
+        <TouchableOpacity style={styles.button} onPress={() => {}}>
+          <Text style={styles.buttonText}>Log In</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  container: { 
+    flex: 1, 
+    justifyContent: 'space-between', 
+    alignItems: 'center', 
+    paddingTop: 80, 
+    backgroundColor: '#2f2f2f'  // Changed to a light gray background
+  },
+  titleContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  title: { 
+    color: '#FFD700',  
+    marginBottom: 20,  
+    fontSize: 150 
+  },
+  inputContainer: {
+    width: '80%',
+    marginBottom: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: { 
+    height: 40, 
+    borderColor: 'gray', 
+    borderWidth: 1, 
+    marginBottom: 20, 
+    width: '80%', 
+    backgroundColor: 'white', 
+    padding: 10, 
+    borderRadius: 5,
+    
+  },
+  button: {
+    backgroundColor: '#FFD700',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: '#000', // text color
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
